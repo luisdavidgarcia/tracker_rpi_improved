2bebf4d6fd6a1f3c5cad13fc14ff098f5e7f1309
Tony Fong <t4fong@uwaterloo.ca>
2021-08-06 15:21:58 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,13 +7,17 @@
     cd "$_path"
     sudo chown pi $_path -R
 
-    echo "Installing python packages"
+    echo "Installing All related packages packages"
     pip3 install numpy
     pip3 install schedule
     pip3 install imutils
     pip3 install wiringpi
     pip3 install tables
-    sudo pip3 install opencv-python==4.1.1.26
+    sudo pip3 install opencv-python==4.1.0.25
+    sudo apt-get install libjasper-dev 
+    sudo apt-get install libatlas-base-dev
+    sudo apt-get install -y libqtgui4
+    sudo apt-get install libqt4-test
     echo "Install complete\n Make sure you manually ENABLE CAMERA PORT."
     exit
 
