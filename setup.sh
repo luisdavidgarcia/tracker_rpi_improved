ddd57ae5b57fdf4c41ec349acde597d1f4e0addf
Tony Fong <t4fong@uwaterloo.ca>
2021-08-06 14:18:52 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,12 +9,11 @@
 
     echo "Installing python packages"
     pip3 install numpy
+    pip3 install schedule
     pip3 install imutils
-    apt install libatlas3-base libsz2 libharfbuzz0b libtiff5 libjasper1 libilmbase12 libopenexr22 libilmbase12 libgstreamer1.0-0 libavcodec57 libavformat57 libavutil55 libswscale4 libqtgui4 libqt4-test libqtcore4
-    pip3 install opencv-contrib-python libwebp6
     pip3 install wiringpi
     pip3 install tables
-
+    sudo pip3 install opencv-python==4.1.1.26
     echo "Install complete\n Make sure you manually ENABLE CAMERA PORT."
     exit
 
