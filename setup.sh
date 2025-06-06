83c2ff466958ea7166f1f5e6bbcc3b2972f8e0ff
Luis David Garcia <hookeri_bartz@aleeas.com>
2025-06-06 11:44:21 -0700

    Add requirements.txt and update setup.sh for package installation

--- setup.sh
+++ setup.sh
@@ -1,24 +1,19 @@
-#$ sudo chmod 765 pi_video_stream_setup.sh
-#$ sudo ./pi_video_stream_setup.sh
-    
-    echo "Changing folder permission to user"
-    _path = $PWD
-    cd "/home/pi"
-    cd "$_path"
-    sudo chown pi $_path -R
+#!/bin/bash
 
-    echo "Installing All related packages packages"
-    pip3 install numpy
-    pip3 install schedule
-    pip3 install imutils
-    pip3 install wiringpi
-    pip3 install tables
-    sudo pip3 install opencv-python==4.1.0.25
-    sudo apt-get install libjasper-dev 
-    sudo apt-get install libatlas-base-dev
-    sudo apt-get install -y libqtgui4
-    sudo apt-get install libqt4-test
-    echo "Install complete\n Make sure you manually ENABLE CAMERA PORT."
-    exit
+echo "Installing System Packages"
+sudo apt-get update
+sudo apt-get install -y python3-venv qt5-default libqt5gui5 libqt5test5
 
+echo "Creating virtual environment"
+python3 -m venv venv
 
+echo "Activating virtual enviroment"
+source venv/bin/activate
+
+echo "Installing Python packages from requirements.txt"
+pip install --upgrade pip
+pip install -r requirements.txt
+
+echo -e "✅ Install complete\n📸 Make sure you manually ENABLE the CAMERA PORT in raspi-config."
+
+exit 0
